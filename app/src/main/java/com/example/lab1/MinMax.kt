25faa682f3cf515eb0087f5849d4351453aeea24
LastSkif <alastskif@gmail.com>
2021-09-15 21:58:29 +0700

    Документация

--- app/src/main/java/com/example/lab1/MinMax.kt
+++ app/src/main/java/com/example/lab1/MinMax.kt
@@ -5,12 +5,18 @@ import android.os.Bundle
 
 class MinMax {
 
+	/**
+	 * Ищет минимальное число из двух переданных значений типа Int
+	**/
 	fun min(a: Int, b: Int) =
 		if(a <= b)
 			a
 		else
 			b
 
+	/**
+	 * Ищет максимальное число из двух переданных значений типа Int
+	 **/
 	fun max(a: Int, b: Int) =
 		if(a >= b)
 			a
